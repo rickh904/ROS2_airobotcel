28927352ef803fe2eb361da30966dd9badce7683
Gerard <GerardAnneHarkema@gmail.com>
2026-06-08 10:51:41 +0200

    Mappenstructuur aangepast en opgeschoond

---
+++ build/voice_control_stijn/share/voice_control_stijn/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/student/workspace/src/ROS2_airobotcel/build/voice_control_stijn"
