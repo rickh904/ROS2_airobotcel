9d9ffb6e6f3fca8de6d8962428be9f403b6340ea
stijndegroott <swp.degroot@student.avans.nl>
2026-06-17 15:39:15 +0200

    Add action clients to voice node

--- voice_control/voice_control/voice_node.py
+++ voice_control/voice_control/voice_node.py
@@ -1,9 +1,11 @@
 import rclpy
 
 from rclpy.node import Node
+from rclpy.action import ActionClient
 
 from std_msgs.msg import String
-
+from airobot_interfaces.action import AutoSort
+from airobot_interfaces.action import SortSpec
 
 
 import serial
@@ -23,6 +25,17 @@ class VoiceNode(Node):
 
             10
 
+        )
+        self.auto_sort_client = ActionClient(
+            self,
+            AutoSort,
+            'auto_sort'
+        )
+
+        self.sort_spec_client = ActionClient(
+            self,
+            SortSpec,
+            'sort_spec'
         )
         self.valid_commands = [
 
@@ -138,7 +151,7 @@ class VoiceNode(Node):
 
                self.publisher_.publish(msg)
 
-
+               self.send_action(command)
 
                self.get_logger().info(f'Gepubliceerd op /voice_command: {command}')
 
@@ -150,19 +163,59 @@ class VoiceNode(Node):
 
             self.serial_port = None
     def extract_command(self, line):
+        if 'robotcommando:' not in line:
+            return None
+
+        command_text = line.split('robotcommando:', 1)[1].strip()
 
         for command in self.valid_commands:
+            if command_text == command:
+               return command
 
-            if command in line:
-
-                return command
-
-
-
-        return None
+       return None
 
 
 
+
+    def send_action(self, command):
+        if command in ['start', 'stop', 'reset']:
+            self.send_auto_sort_goal(command)
+            return
+
+        product_map = {
+            'pick_oral_b_head': 'oral_b_head',
+            'pick_aaa_battery': 'aaa_battery',
+            'pick_m6_bolt': 'm6_bolt',
+            'pick_wall_plug': 'wall_plug',
+        }
+
+        if command in product_map:
+            self.send_sort_spec_goal(product_map[command])
+            return
+
+        self.get_logger().warn(f'Geen action gekoppeld aan command: {command}')
+
+    def send_auto_sort_goal(self, command):
+        if not self.auto_sort_client.wait_for_server(timeout_sec=1.0):
+            self.get_logger().error('Action server auto_sort niet beschikbaar')
+            return
+
+        goal_msg = AutoSort.Goal()
+        goal_msg.command = command
+
+        self.get_logger().info(f'Verstuur auto_sort action goal: {command}')
+        self.auto_sort_client.send_goal_async(goal_msg)
+
+    def send_sort_spec_goal(self, product_type):
+        if not self.sort_spec_client.wait_for_server(timeout_sec=1.0):
+            self.get_logger().error('Action server sort_spec niet beschikbaar')
+            return
+
+        goal_msg = SortSpec.Goal()
+        goal_msg.product_type = product_type
+
+        self.get_logger().info(f'Verstuur sort_spec action goal: {product_type}')
+        self.sort_spec_client.send_goal_async(goal_msg)
 
 
 def main(args=None):
