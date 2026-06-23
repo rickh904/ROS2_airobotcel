a3e0c7fcb65c4c4d9688a490d4e1ce2705f2f6fc
Gerard <GerardAnneHarkema@gmail.com>
2026-06-23 14:55:44 +0200

    1.4.0

--- manipulator_unit/setup.py
+++ manipulator_unit/setup.py
@@ -7,7 +7,7 @@ package_name = 'manipulator_unit'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=['manipulator_unit'],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
--- my_uf_description/setup.py
+++ my_uf_description/setup.py
@@ -12,10 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        # Aangepast: Neemt nu ALLES mee uit de urdf map, dus ook .STL en .stl bestanden
-        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*'))),
-        (os.path.join('share', package_name, 'cfg'), glob(os.path.join('cfg', '*.*')))
+        # Directe paden werken in ROS 2 ament_python vaak betrouwbaarder met glob
+        ('share/' + package_name + '/launch', glob('launch/*')),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
+        ('share/' + package_name + '/cfg', glob('cfg/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +23,6 @@ setup(
     maintainer_email='GA.Harkema@avans.nl',
     description='TODO: Package description',
     license='TODO: License declaration',
-    #tests_require=['pytest'],
     entry_points={
         'console_scripts': [
         ],
